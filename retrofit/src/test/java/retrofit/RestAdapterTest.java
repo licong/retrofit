93c8a3effc654b748b23cdf48b28343f206480f6
Jake Wharton <jw@squareup.com>
2015-04-16 17:57:37 -0400

    Remove 'set' prefix from builder methods.

--- retrofit/src/test/java/retrofit/RestAdapterTest.java
+++ retrofit/src/test/java/retrofit/RestAdapterTest.java
@@ -49,9 +49,9 @@ public class RestAdapterTest {
     OkHttpClient client = new OkHttpClient();
 
     example = new RestAdapter.Builder() //
-        .setClient(client)
-        .setCallbackExecutor(new SynchronousExecutor())
-        .setEndpoint(server.getUrl("/").toString())
+        .client(client)
+        .callbackExecutor(new SynchronousExecutor())
+        .endpoint(server.getUrl("/").toString())
         .build()
         .create(Example.class);
   }
@@ -64,7 +64,7 @@ public class RestAdapterTest {
 
   @Test public void interfaceWithExtendIsNotSupported() {
     try {
-      new RestAdapter.Builder().setEndpoint("http://foo/").build().create(InvalidExample.class);
+      new RestAdapter.Builder().endpoint("http://foo/").build().create(InvalidExample.class);
       fail();
     } catch (IllegalArgumentException e) {
       assertThat(e).hasMessage("Interface definitions must not extend other interfaces.");
